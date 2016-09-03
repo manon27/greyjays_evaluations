01c42a2dadf7a31614261b6474c4f2bfbb72b223
manolo72 <manoel.olivier@gmail.com>
2016-09-03 19:18:01 +0200

    ajout de comm

--- src/app/components/crudPosition/crudPosition.directive.js
+++ src/app/components/crudPosition/crudPosition.directive.js
@@ -5,6 +5,13 @@
 		.module('greyjays.evaluations')
 		.directive("crudPosition", crudPosition);
 
+	/**
+	@name		crudPosition
+	@desc 		<crud-position items="" le-service=""></crud-position>
+	@params		rootScope pour appeler la fonction refresh du scope root
+	@returns	GUI de gestion CRUD des positions
+	*/
+
 	/** @ngInject */
 	function crudPosition($rootScope) {
 		var directive = {
@@ -19,6 +26,7 @@
 		return directive;
 		
 		function linkF(scope) {
+
 			scope.affichage={};
 			scope.affichage.add=false;
 			scope.affichage.upd=false;
@@ -26,6 +34,21 @@
 			scope.items = scope.items || [];
 			scope.itemAdd = {};
 			
+			/**
+			@name	afficherAjout
+			@desc 	affichage de la GUI d'ajout avec init du param
+			*/
+			scope.afficherAjout = function() {
+				scope.itemAdd = {};
+				scope.affichage.add=true;
+				scope.affichage.upd=false;
+			};
+
+			/**
+			@name		afficherModification
+			@desc 		affichage de la GUI de modif
+			@params 	it : item de position
+			*/
 			scope.afficherModification = function(it) {
 				scope.itemAdd.id = it.id;
 				scope.itemAdd.libelle = it.libelle;
@@ -33,11 +56,11 @@
 				scope.affichage.add=false;
 				scope.affichage.upd=true;
 			};
-			scope.afficherAjout = function() {
-				scope.itemAdd = {};
-				scope.affichage.add=true;
-				scope.affichage.upd=false;
-			};
+
+			/**
+			@name		enregistrer
+			@desc 		appel du service save + refresh via la root
+			*/
 			scope.enregistrer = function() {
 				var itemAjout = {};
 				itemAjout.libelle = scope.itemAdd.libelle;
@@ -52,10 +75,20 @@
 				scope.affichage.upd=false;
 				
 			};
+
+			/**
+			@name 	annuler
+			@desc 	Masquer les interfaces add et upd
+			*/
 			scope.annuler = function() {
 				scope.affichage.add=false;
 				scope.affichage.upd=false;
 			};
+
+			/**
+			@name 	effacer
+			@desc 	Appel du service (service.delete) + actualisation par le root ($rootScope.$broadcast)
+			*/
 			scope.effacer = function(itemDel) {
 				scope.leService.delete(itemDel.id).then(function() {
 					$rootScope.$broadcast('refresh');
