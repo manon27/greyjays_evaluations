d05524da21dc4a049889d504f3afb14c7f77928d
manolo72 <manoel.olivier@gmail.com>
2016-09-03 19:43:23 +0200

    ajout de comm

--- src/app/filters/phpcrudapitransform/phpcrudapitransform.filter.js
+++ src/app/filters/phpcrudapitransform/phpcrudapitransform.filter.js
@@ -5,6 +5,13 @@
 		.module('greyjays.evaluations')
 		.filter('php_crud_api_transform', php_crud_api_transform);
 
+	/**
+	@name 		php_crud_api_transform
+	@desc 		filtre de transformation du json issu de l'API en json pour l'appli
+	@param 		librairie underscore
+	@returns	objet json
+	*/
+
 	/** @ngInject */
 	function php_crud_api_transform(_) {
 		return function (input) {
@@ -27,4 +34,24 @@
 			return newJson;
 		};
 	}
+
+	/**
+	--------------------------------------------------
+	structure du json retourné par l'API
+	--------------------------------------------------
+		position
+			columns ["id","libelle","description","date_creation","date_modification"]
+			records [
+				["1","Ma position 1","C'est la position 1s","2016-08-31 22:14:31","2016-08-31 22:14:31"]
+				["2","Position 2","C'est la seconde position","2016-08-31 22:15:14","2016-08-31 22:15:14"]
+				...
+			]
+	--------------------------------------------------
+	structure du json utilisé par AngularJs
+	--------------------------------------------------
+		positions [
+			{id:1,libelle:"Ma position 1", description:"C'est la position 1s", date_creation:"2016-08-31 22:14:31",}
+			...
+		]
+	*/
 })();
