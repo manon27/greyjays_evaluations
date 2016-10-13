8b9d31655a1621b45098a6f7f7482aa1cc8d14fb
manolo72 <manoel.olivier@gmail.com>
2016-10-13 23:43:55 +0200

    gui du détail de joueur avec graphes

--- src/app/components/detailJoueur/detailJoueur.directive.js
+++ src/app/components/detailJoueur/detailJoueur.directive.js
@@ -13,7 +13,7 @@
 	 */
 
 	/** @ngInject */
-	function detailJoueur(PositionService, _) {
+	function detailJoueur($filter, PositionService, _) {
 		var directive = {
 			restrict: 'E',
 			scope: {
@@ -26,30 +26,6 @@
 		
 		function linkF(scope) {
 
-			scope.allLabels = [];
-			scope.allTitres = [];
-			scope.allDonnees = [];
-			scope.allOptions = {
-				legend: {
-					display: true
-				},
-				scale: {
-					ticks: {beginAtZero: true, min: 0, max: 5}
-				}
-			};
-			scope.allColors = ['#FF0000','#0063AC','#7AB51D'];
-			scope.allSeries = ['Max du joueur', 'Moyenne du joueur', 'Moyenne équipe'];
-			scope.allDSOverride = [
-				{
-					backgroundColor: "rgba(0,0,0,0)"
-				},
-				{
-					backgroundColor: "rgba(0,0,0,0)"
-				},
-				{
-					backgroundColor: "rgba(0,0,0,0)"
-				}
-			];
 
 			/**
 			 * Surveiller les modifications sur le joueur (qui)
@@ -57,15 +33,50 @@
 			 */	
 			scope.$watch('qui', function(newJoueur) {
 
-				scope.allLabels = [];
-				scope.allTitres = [];
-				scope.allDonnees = [];
+				construireRadar(newJoueur);
+
+				construireLine(newJoueur);
+
+			});
+
+			var construireRadar = function(objJoueur) {
+
+				scope.mesradars = {
+					allOptions : {
+						legend: {
+							display: true
+						},
+						scale: {
+							ticks: {beginAtZero: true, min: 0, max: 5, stepSize: 1}
+						}
+					},
+					allColors : ['#D0262E', '#3F2992', '#D4B427'],
+					allSeries : ['Max du joueur', 'Moyenne du joueur', 'Moyenne équipe'],
+					allDSOverride : [
+						{
+							backgroundColor: "rgba(0,0,0,0.1)"
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0.1)"
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0.1)"
+						}
+					],
+					allGraphs : []
+				};
 
 				// radars en fonction des positions...
 				var allPos = PositionService.all;
 				
 				_.each(allPos, function(maPos) {
-					scope.allTitres.push(maPos.libelle + ' : ' + maPos.description);
+					var unGraph = {
+						ind: 'radar'+maPos.id,
+						monTitre : maPos.libelle + ' : ' + maPos.description,
+						mesLabels: [],
+						mesDonnees: []
+					};
+
 					var labels = [];
 					var donnees = [];
 					var donneesMax = [];
@@ -74,9 +85,12 @@
 					_.each(maPos.actions, function(monAct) {
 						labels.push(monAct.libelle);
 						//	sur le joueur en cours...
-						var res = _.filter(newJoueur.resultats, function(result) {
-							return result.id_action === monAct.id;
-						});
+						var res = [];
+						if (typeof objJoueur.resultats !== 'undefined') {
+							res = _.filter(objJoueur.resultats, function(result) {
+								return result.id_action === monAct.id;
+							});
+						}
 						var max = 0;
 						var moy = 0;
 						if (res.length > 0) {
@@ -86,7 +100,7 @@
 							_.each(res, function(item) {
 								moy += item.maNote.length;
 							});
-							moy = moy / res.length;
+							moy = Math.round(moy / res.length * 100) / 100;
 						}
 						donneesMax.push(max);
 						donneesMoy.push(moy);
@@ -97,18 +111,146 @@
 							_.each(monAct.resultats, function(res0) {
 								totEquipe += res0.maNote.length;
 							});
-							moyEquipe = totEquipe / monAct.resultats.length;
+							moyEquipe = Math.round(totEquipe / monAct.resultats.length * 100) / 100;
 						}
 						donneesMoyEquipe.push(moyEquipe);
 					});
-					scope.allLabels.push(labels);
+					unGraph.mesLabels = labels;
 					donnees.push(donneesMax);
 					donnees.push(donneesMoy);
 					donnees.push(donneesMoyEquipe);
-					scope.allDonnees.push(donnees);
+					unGraph.mesDonnees = donnees;
+					scope.mesradars.allGraphs.push(unGraph);
 				});
 
-			});
+			};
+
+			var construireLine = function(objJoueur) {
+
+				scope.mesLines = {
+					allOptions : {
+						legend: {
+							display: true
+						},
+						scales: {
+							xAxes: [{
+								display: false
+							}],
+							yAxes: [{
+								ticks: {
+									max: 5,
+									min: 0,
+									stepSize: 1
+								}
+							}]
+						}
+					},
+					allColors : ['#D4B427', '#3F2992', '#D0262E', '#2DAD20', '#333333'],
+					allDSOverride : [
+						{
+							backgroundColor: "rgba(0,0,0,0)",
+							spanGaps: true,
+							lineTension: 0
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0)",
+							spanGaps: true,
+							lineTension: 0
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0)",
+							spanGaps: true,
+							lineTension: 0
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0)",
+							spanGaps: true,
+							lineTension: 0
+						},
+						{
+							backgroundColor: "rgba(0,0,0,0)",
+							spanGaps: true,
+							lineTension: 0
+						}
+					],
+					allGraphs : []
+				};
+
+				// radars en fonction des positions...
+				var allPos = PositionService.all;
+				
+				_.each(allPos, function(maPos) {
+					var unGraph = {
+						ind: 'line'+maPos.id,
+						monTitre : maPos.libelle + ' : ' + maPos.description,
+						mesSeries : [],
+						mesLabels : [],
+						mesDonnees : []
+					};
+
+					var series = [];
+					var labels = [];
+
+					_.each(maPos.actions, function(monAct) {
+						series.push(monAct.libelle);
+
+						//	sur le joueur en cours...
+						var res = [];
+						if (typeof objJoueur.resultats !== 'undefined') {
+							res = _.filter(objJoueur.resultats, function(result) {
+								return result.id_action === monAct.id;
+							});
+						}
+						if (res.length > 0) {
+							res = _.sortBy(res,'date_realisation');
+							_.each(res, function(point) {
+								labels.push(new Date(point.date_realisation).getTime());
+							});
+						}
+					});
+					labels = labels.sort();
+
+					var donnees = [];
+					_.each(maPos.actions, function(monAct) {
+						var donneesAct = [];
+						//	sur le joueur en cours...
+						var res = [];
+						if (typeof objJoueur.resultats !== 'undefined') {
+							res = _.filter(objJoueur.resultats, function(result) {
+								return result.id_action === monAct.id;
+							});
+						}
+						if (res.length > 0) {
+							res = _.sortBy(res,'date_realisation');
+							_.each(res, function(point) {
+								for (var i=0; i<labels.length; i++) {
+									if (labels[i] == new Date(point.date_realisation).getTime()) {
+										donneesAct[i] = point.maNote.length;
+									}
+								}
+								labels.push();
+							});
+						}
+						for (var i=0; i<labels.length; i++) {
+							if (!donneesAct[i]) {
+								donneesAct[i]=null;
+							}
+						}
+						donnees.push(donneesAct);
+					});
+
+					for (var i=0; i<labels.length; i++) {
+						var tt = new Date(labels[i]);
+						labels[i] = $filter('formaterDate')(tt,'JJ/MM/AAAA à HHhMM');
+					}
+					
+					unGraph.mesSeries = series;
+					unGraph.mesLabels = labels;
+					unGraph.mesDonnees = donnees;
+					scope.mesLines.allGraphs.push(unGraph);
+				});
+
+			};
 
 		}
 	}
