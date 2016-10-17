87c18af63a7715be28605bef771ba0489dd18ebf
manolo72 <manoel.olivier@gmail.com>
2016-10-17 19:13:55 +0200

    champ description transformé en html avec br

--- src/app/filters/contexte/contexte.filter.js
+++ src/app/filters/contexte/contexte.filter.js
@@ -11,12 +11,14 @@
 			var str = '';
 			if (input === 0) {
 				if (champs === 'inmatch') str += 'Entraînement';
-				if (champs === 'mesurable') str += 'Non';
+				if (champs === 'mesurable') str += 'L\'action n\'est pas dénombrable (mesurée par un nombre).';
 			} else if (input === 1) {
 				if (champs === 'inmatch') str += 'Match';
-				if (champs === 'mesurable') str += 'Oui (+)';
+				if (champs === 'mesurable') str += 'La note augmente si la mesure augmente (ex: distance).';
 			} else if (input === 2) {
-				if (champs === 'mesurable') str += 'Oui (-)';
+				if (champs === 'mesurable') str += 'La note augmente si la mesure diminue (ex: chrono).';
+			} else {
+				str += input.replace(/(?:\r\n|\r|\n)/g, '<br />');
 			}
 			return str;
 		};
