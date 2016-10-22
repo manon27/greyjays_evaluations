39661777f0fad0701b7508dc307d0f29b38feb11
manolo72 <manoel.olivier@gmail.com>
2016-10-23 01:20:20 +0200

    ajout du fichier de conf

---
+++ server/conf.php
@@ -0,0 +1,19 @@
+<?php
+# Utilisateur MySQL
+define('DB_USER','manoloexpo');
+
+# Mot de passe MySQL
+define('DB_PASS','carbone72');
+
+# Nom de machine MySQL (généralement localhost)
+define('DB_HOST','sql.free.fr');
+
+# Nom de la base de données
+define('DB_DBASE','manoloexpo');
+
+# Clé de session
+define('GJ_SESSION','*********');
+
+# Mot de passe de session
+define('GJ_PWD_SESSION','*********');
+?>
